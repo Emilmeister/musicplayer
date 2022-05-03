f44e4dab22dc42036fb1e5f43bba685661179f60
e.madatov <emilis.highscreen@gmail.com>
2022-05-03 21:06:45 +0300

    bug fix

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,3 @@
-import FilePicker.FilePickerView;
-import components.MusicList.MusicListView;
-import components.Player.PlayerView;
-import components.Window;
 import config.SpringConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
